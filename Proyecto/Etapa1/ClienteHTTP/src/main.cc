cf53f5161f47c30dc182e20662d77589d5f9dcea
CarlosAlvaradoM <ca899211@gmail.com>
2026-03-23 16:17:44 -0600

    main.cc: ajuste

--- Proyecto/Etapa1/ClienteHTTP/src/main.cc
+++ Proyecto/Etapa1/ClienteHTTP/src/main.cc
@@ -1,17 +1,7 @@
-/**
-  *  Universidad de Costa Rica
-  *  ECCI
-  *  CI0123 Proyecto integrador de redes y sistemas operativos
-  *
-  *
-  *  Cliente
-  *
- **/
-
 #include <stdio.h>
 #include <string.h>
 
-#include "VSocket.h"
+#include "Parser.h"
 #include "Socket.h"
 #include "Menu.h"
 
@@ -31,23 +21,32 @@ int main( int argc, char * argv[] ) {
    // Elejir parte de la figura
    int parteFiguraUsuario = elegirParte();
 
+   const char* figura = obtenerFigura(figuraUsuario);
+
    // Hacer el request con datos del usuario
    construirRequest(request, obtenerFigura(figuraUsuario), parteFiguraUsuario);
 
+   // Socket
    s = new Socket( 's' );
-
    s->Connect( ose, 80 );	// usar "osi" en la ECCI, "ose" de sus casas
-
    s->Write(request);
 
+   // Leer respuesta
+   std::string respuesta;
    int bytesRead;
 
    while ( (bytesRead = s->Read( buffer, 511 )) > 0 ) {
-      buffer[bytesRead] = '\0'; // Aseguramos que la cadena termine correctamente
-      printf( "%s", buffer);
+      buffer[bytesRead] = '\0';
+      respuesta += buffer;
    }
 
    delete s;
+
+   // Extraer html
+   std::string html = extraerHTML(respuesta);
+
+   // Mostrar los bricks
+   imprimirBricks(html, parteFiguraUsuario, figura);
    return 0;
 
 }
