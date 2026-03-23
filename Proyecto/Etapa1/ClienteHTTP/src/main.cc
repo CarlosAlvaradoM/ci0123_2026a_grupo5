6e1b5f2dfbdace5baf97b341b9d5bab316420def
CarlosAlvaradoM <ca899211@gmail.com>
2026-03-23 02:30:35 -0600

    main.cc: Ajuste main por menu

--- Proyecto/Etapa1/ClienteHTTP/src/main.cc
+++ Proyecto/Etapa1/ClienteHTTP/src/main.cc
@@ -13,30 +13,42 @@
 
 #include "VSocket.h"
 #include "Socket.h"
+#include "Menu.h"
 
 int main( int argc, char * argv[] ) {
    const char * os = "http://os.ecci.ucr.ac.cr/";
    const char * osi = "10.84.166.62";
    const char * ose = "163.178.104.62";
-   const char * elephant = "GET /lego/list.php?figure=elephant&part=1 HTTP/1.1\r\n"
-                        "Host: os.ecci.ucr.ac.cr\r\n"
-                        "\r\n";
-
 
    VSocket * s;	
-   char a[512];
+   char buffer[512];
+   char request[512];
+
+   // Mostrar el menu
+   mostrarMenu();
+   // Elejir figura
+   int figuraUsuario = elegirFigura();
+   // Elejir parte de la figura
+   int parteFiguraUsuario = elegirParte();
+
+   // Hacer el request con datos del usuario
+   construirRequest(request, obtenerFigura(figuraUsuario), parteFiguraUsuario);
 
    s = new Socket( 's' );
+
    s->Connect( ose, 80 );	// usar "osi" en la ECCI, "ose" de sus casas
-   s->Write( elephant );
+
+   s->Write(request);
+
    int bytesRead;
-   while ( (bytesRead = s->Read( a, 511 )) > 0 ) {
-      a[bytesRead] = '\0'; // Aseguramos que la cadena termine correctamente
-      printf( "%s", a);
+
+   while ( (bytesRead = s->Read( buffer, 511 )) > 0 ) {
+      buffer[bytesRead] = '\0'; // Aseguramos que la cadena termine correctamente
+      printf( "%s", buffer);
    }
-   /*
-   s->Read( a, 512 );
-   printf( "%s\n", a);
-   */
+
+   delete s;
+   return 0;
 
 }
+
