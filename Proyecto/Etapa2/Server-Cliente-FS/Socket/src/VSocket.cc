b38a5494b4a45e8fa4c9328bff7f454f348d096e
kiara-brenes <kiara.brenesdiaz@ucr.ac.cr>
2026-04-26 23:17:28 -0600

    Update VSocket.cc

--- Proyecto/Etapa2/Server-Cliente-FS/Socket/src/VSocket.cc
+++ Proyecto/Etapa2/Server-Cliente-FS/Socket/src/VSocket.cc
@@ -1,15 +1,4 @@
-/**
-  *  Universidad de Costa Rica
-  *  ECCI
-  *  CI0123 Proyecto integrador de redes y sistemas operativos
-  *  2026-i
-  *  Grupos: 2 y 3
-  *
-  ****** VSocket base class implementation
-  *
-  * (Fedora version)
-  *
- **/
+// Copyright 2026a Equipo 1 CI-0123. ECCI-UCR. CC BY 4.0
 
 #include <sys/socket.h>
 #include <arpa/inet.h>		// ntohs, htons
@@ -17,25 +6,10 @@
 #include <cstring>		// memset
 #include <netdb.h>		// getaddrinfo, freeaddrinfo
 #include <unistd.h>		// close
-/*
-#include <cstddef>
-#include <cstdio>
 
-//#include <sys/types.h>
-*/
 #include "VSocket.h"
 
 
-/**
-  *  Class creator (constructor)
-  *     use Unix socket system call
-  *
-  *  @param     char t: socket type to define
-  *     's' for stream
-  *     'd' for datagram
-  *  @param     bool ipv6: if we need a IPv6 socket
-  *
- **/
 void VSocket::Init( char t, bool IPv6 ){
 
    this->type = t;
@@ -53,13 +27,6 @@ void VSocket::Init( char t, bool IPv6 ){
 }
 
 
-/**
-  *  Class creator (constructor)
-  *     use Unix socket system call
-  *
-  *  @param     int id: socket identifier
-  *
- **/
 void VSocket::Init( int id ){
 
    this->sockId = id;
@@ -70,20 +37,14 @@ void VSocket::Init( int id ){
 
 }
 
-/**
-  * Class destructor
-  *
- **/
+
 VSocket::~VSocket() {
 
    this->Close();
 
 }
 
-/**
-  * Class destructor
-  *
- **/
+
 void VSocket::Close(){
 
    if ( this->sockId >= 0 ) {
@@ -96,15 +57,6 @@ void VSocket::Close(){
 }
 
 
-
-/**
-  * TryToConnect method
-  *   use "connect" Unix system call
-  *
-  * @param      char * host: host address in dot notation, example "10.84.166.62"
-  * @param      int port: process address, example 80
-  *
- **/
 int VSocket::TryToConnect(const char *hostip, int port) {
 
    if (this->IPv6) {
@@ -135,31 +87,14 @@ int VSocket::TryToConnect(const char *hostip, int port) {
 }
 
 
-/**
-  * TryToConnect method
-  *   use "connect" Unix system call
-  *
-  * @param      char * host: host address in dns notation, example "os.ecci.ucr.ac.cr"
-  * @param      char * service: process address, example "http"
-  *
- **/
 int VSocket::TryToConnect( const char *host, const char *service ) {
 
-   int port = atoi(service);   // convierte "8080" → 8080
+   int port = atoi(service);
 
    return this->TryToConnect(host, port);
 }
 
 
-/**
-  * Bind method
-  *    use "bind" Unix system call (man 3 bind) (server mode)
-  *
-  * @param      int port: bind a unamed socket to a port defined in sockaddr structure
-  *
-  *  Links the calling process to a service at port
-  *
- **/
 int VSocket::Bind(int port) {
 
    this->port = port;
@@ -186,15 +121,6 @@ int VSocket::Bind(int port) {
 }
 
 
-/**
-  * MarkPassive method
-  *    use "listen" Unix system call (man listen) (server mode)
-  *
-  * @param      int backlog: defines the maximum length to which the queue of pending connections for this socket may grow
-  *
-  *  Establish socket queue length
-  *
- **/
 int VSocket::MarkPassive( int backlog ) {
 
    int st = listen(this->sockId, backlog);
@@ -207,14 +133,6 @@ int VSocket::MarkPassive( int backlog ) {
 }
 
 
-/**
-  * WaitForConnection method
-  *    use "accept" Unix system call (man 3 accept) (server mode)
-  *
-  *
-  *  Waits for a peer connections, return a sockfd of the connecting peer
-  *
- **/
 int VSocket::WaitForConnection( void ) {
 
    struct sockaddr_in addr;
@@ -229,14 +147,7 @@ int VSocket::WaitForConnection( void ) {
    return client;
 }
 
-/**
-  * Shutdown method
-  *    use "shutdown" Unix system call (man 3 shutdown) (server mode)
-  *
-  *
-  *  cause all or part of a full-duplex connection on the socket associated with the file descriptor socket to be shut down
-  *
- **/
+
 int VSocket::Shutdown( int mode ) {
 
    int st = shutdown(this->sockId, mode);
@@ -249,18 +160,6 @@ int VSocket::Shutdown( int mode ) {
 }
 
 
-// UDP methods 2025
-
-/**
-  *  sendTo method
-  *
-  *  @param	const void * buffer: data to send
-  *  @param	size_t size data size to send
-  *  @param	void * addr address to send data
-  *
-  *  Send data to another network point (addr) without connection (Datagram)
-  *
- **/
 size_t VSocket::sendTo( const void * buffer, size_t size, void * addr ) {
    int st = -1;
 
@@ -269,18 +168,6 @@ size_t VSocket::sendTo( const void * buffer, size_t size, void * addr ) {
 }
 
 
-/**
-  *  recvFrom method
-  *
-  *  @param	const void * buffer: data to send
-  *  @param	size_t size data size to send
-  *  @param	void * addr address to receive from data
-  *
-  *  @return	size_t bytes received
-  *
-  *  Receive data from another network point (addr) without connection (Datagram)
-  *
- **/
 size_t VSocket::recvFrom( void * buffer, size_t size, void * addr ) {
    int st = -1;
 
